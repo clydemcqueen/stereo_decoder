b03c131fc559aae39cf2ce91c5662206e6f166f0
Clyde McQueen <clyde@mcqueen.net>
2021-10-07 10:48:59 -0700

    Add license and readme info

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
-Requires h264_image_transport
+## stereo_decoder
 
-Need to provide 2 H.264 streams
+Demo multi-threaded stereo H.264 decoder library for [ROS2 Foxy](https://docs.ros.org/en/foxy/).
 
+### Features
+* decodes [h264 messages](https://github.com/clydemcqueen/h264_image_transport) and returns
+[image messages](https://index.ros.org/p/sensor_msgs/) 
+* ignores timestamps, doesn't use message filters, just keeps the last stereo pair
+
+### Example use
+* subscribes to `stereo/left/image_raw/h264` and `stereo/right/image_raw/h264`
+* publishes `stereo/left/image_raw` and `stereo/right/image_raw` at 1Hz
+
+~~~
 ros2 run stereo_decoder stereo_decoder_node
+~~~
+
+### Requirements
+
+Tested on ROS2 Foxy (Ubuntu 20.04).
+
+Requires [h264 messages](https://github.com/clydemcqueen/h264_image_transport).
+
+Required libraries:
+* libavdevice>=58
+* libavformat>=58
+* libavcodec>=58
+* libavutil>=56
+* libswscale>=5
+
+Here's one way to install these libraries:
+~~~
+sudo apt install libavdevice-dev libavformat-dev libavcodec-dev libavutil-dev libswscale-dev
+~~~
+
+Note: `rosdep` won't find keys for most of these libraries so `package.xml` declares
+dependencies on `ffmpeg` and `libavdevice-dev`. Strictly speaking `ffmpeg` is not required.
--- src/stereo_decoder.cpp
+++ src/stereo_decoder.cpp
@@ -1,4 +1,27 @@
+// MIT License
+//
+// Copyright (c) 2021 Clyde McQueen
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <iostream>
+
 #include "stereo_decoder/stereo_decoder.hpp"
 
 namespace stereo_decoder
@@ -6,7 +29,7 @@ namespace stereo_decoder
 
 StereoDecoder::StereoDecoder()
 {
-  // TODO -> Worker class
+  // Could move thread to a Worker class one day...
   left_thread_ = std::thread([this]()
   {
     while (true) {
@@ -134,7 +157,6 @@ void StereoDecoder::stop()
   lock.unlock();
   condition_.notify_all();
 
-  // TODO -> Worker class
   if (left_thread_.joinable()) {
     left_thread_.join();
   }
